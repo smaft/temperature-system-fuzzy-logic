c171cde662c23ac44ccb770fe359928841e849ca
Aleksandra Zajączkowska <aleksandrazajaczkowska1@gmail.com>
2019-12-07 12:58:25 +0100

    Drobne zmiany

--- TemperatureSystem/src/UserInterface.java
+++ TemperatureSystem/src/UserInterface.java
@@ -3,12 +3,16 @@ import java.awt.*;
 
 public class UserInterface {
 
-    private JFrame window = new JFrame("Sterowanie temperaturą");
+    private JFrame window = new JFrame("HEATING - AIR CONDITIONING SYSTEM");
+    // Menu
     private JMenuBar menuBar = new JMenuBar();
     private JMenu control = new JMenu("Kontrola");
     private JMenu help = new JMenu("Pomoc");
-    private JMenuItem c1, c2, h1, h2;
-
+    private JMenuItem c1 = new JMenuItem("Ustaw plan dnia");
+    private JMenuItem c2 = new JMenuItem("Pokaż wykresy");
+    private JMenuItem c3 = new JMenuItem("Wyjście z aplikacji");
+    private JMenuItem h1 = new JMenuItem("Korzystanie z aplikacji");
+    private JMenuItem h2 = new JMenuItem("O autorach");
     // Panel kontrolny użytkownika, GridLayout(2, 3)
     private JPanel userPanel = new JPanel();
     // Pierwszy wiersz
@@ -34,11 +38,17 @@ public class UserInterface {
         window.setLocationRelativeTo(null);
         window.setLayout(new FlowLayout());
         window.setMinimumSize(new Dimension(600,300));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Menu
         window.setJMenuBar(menuBar);
         menuBar.add(control);
         menuBar.add(help);
+        control.add(c1);
+        control.add(c2);
+        control.add(c3);
+        help.add(h1);
+        help.add(h2);
 
         // Panel sterowania uzytkownika
         window.add(userPanel);
@@ -65,8 +75,8 @@ public class UserInterface {
                 showInvalidDialog("Wprowadź wartość numeryczną, np. 22 lub 23.5");
             }
             Float exceptedTemp = Float.valueOf(getExcTemp);
-            if (exceptedTemp < 15 || exceptedTemp > 25) {
-                showInvalidDialog("Wprowadź temperaturę z zakresu 15-25 stopni!");
+            if (exceptedTemp < 18 || exceptedTemp > 28) {
+                showInvalidDialog("Wprowadź temperaturę z zakresu 18-28 stopni!");
             }
             else {
                 showConfirmDialog();
@@ -92,7 +102,12 @@ public class UserInterface {
     }
 
     public static void main(String[] args) {
-        new UserInterface();
+        // W watku, aby uniknac zakleszczen przy tworzeniu GUI
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new UserInterface();
+            }
+        });
     }
 
 }
